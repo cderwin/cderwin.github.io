db448ccd5956c8a3b403c0f9f00d7fcfe476f542
Cameron Derwin <camderwin@gmail.com>
2016-01-11 07:52:01 +0000

    Update deploy script and don't publish emty .md files

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ if [[ $JEKYLL_ENV =~ PROD ]]; then
     rm -rf $DEPLOYDIR/* && mkdir -p $DEPLOYDIR
 
     echo "Copying build artifact to $DEPLOYDIR..."
-    make -C $SRCDIR build && rsync -r $BUILDDIR/* $(deploydir)/
+    make -C $SRCDIR build && rsync -r $BUILDDIR/* $DEPLOYDIR/
 else
     echo "Connecting to deploy server..."
     ssh ${1-"hub"} JEKYLL_ENV=PRODUCTION source $SRCDIR/deploy.sh
